6729385657b9cba240f24eaab783989e5b9238c4
Ian <chemwenokibet11@gmail.com>
2023-01-24 14:45:55 +0300

    Created a useEffect function to fetch data from the API

--- src/components/Memes.js
+++ src/components/Memes.js
@@ -9,5 +9,12 @@ function Memes() {
         randomImage: "https://i.imgflip.com/1g8my4.jpg"
     }, []
     )
-    
+    const[allMemes, setALlMemes] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setALlMemes(data.data.memes))
+    }, [])
 }
+export default Memes
