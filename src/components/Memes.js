dd25592e2972b54b400709eebb844d5699b3e52b
Ian <chemwenokibet11@gmail.com>
2023-01-24 14:40:52 +0300

    Created a function with memes set in state

---
+++ src/components/Memes.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import { useEffect } from "react";
+
+
+function Memes() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/1g8my4.jpg"
+    }, []
+    )
+    
+}
