e19633667cbb36f5b84460972b4f53520a79af9f
Ernest <ernest.cheruiyot@student.moringaschool.com>
2023-01-25 12:59:36 +0300

    add home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,36 @@ import "../index.css";
 function Home(){
     return (
         <div>
-            <div class="head">
-            <div class="row">
-              <div class="col-md-12 text-center">
-                <h3 class="animate-charcter"> Meme App!</h3>
+            <div className="head">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h3 className="animate-charcter"> Meme App!</h3>
               </div>
             </div>
           </div>
+
+          
+
+
+
+
+
+          
+        <div className="holder">
+            <div className="carousel">
+                <div className="image"><span>Memes</span></div>
+                <div className="image"><span>Laugh</span></div>
+                <div className="image"><span>Hilarious</span></div>
+                <div className="image"><span>fun</span></div>
+                <div className="image"><span>Smile</span></div>
+                <div className="image"><span>Thrill</span></div>
+                <div className="image"><span>Follow</span></div>
+                <div className="image"><span>Like</span></div>
+                <div className="image"><span>Comment</span></div>
+            </div>
+        </div>
+
+
         </div>
     )
 }
