e0f32fca5bb6311f35f915cdd3bb82f01c5b8242
Ernest <ernest.cheruiyot@student.moringaschool.com>
2023-01-25 11:35:12 +0300

    add home.js

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../index.css";
+function Home(){
+    return (
+        <div>
+            <div class="head">
+            <div class="row">
+              <div class="col-md-12 text-center">
+                <h3 class="animate-charcter"> Meme App!</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default Home;
